399f6dc424afd9d2cc793545ddde7774f515860e
段体华 <duantihua@gmail.com>
2021-05-10 16:05:04 +0800

    update transport version

--- transport/src/main/scripts/transport.sh
+++ transport/src/main/scripts/transport.sh
@@ -47,7 +47,7 @@ export slf4j_ver=2.0.0-alpha1
 export logback_ver=1.3.0-alpha5
 export commons_compress_ver=1.19
 export boot_ver=0.0.20
-export beangle_db_ver=0.0.9
+export beangle_db_ver=0.0.10
 
 download org.scala-lang scala-library $scala_ver
 download org.scala-lang scala-reflect $scala_ver
@@ -77,3 +77,4 @@ if [[ -f $jarfile ]];then
 else
   echo "Cannot find $jarfile,Transportation aborted."
 fi
+
