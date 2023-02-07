0c1b1380a18cd77c9ac4cc9cf9ee6adf3605dadd
段体华 <duantihua@gmail.com>
2023-02-07 12:40:13 +0800

    update to parent 0.8.0

--- transport/src/main/scripts/transport.sh
+++ transport/src/main/scripts/transport.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 if [ $# -eq 0 ]; then
   echo "Usage:
    transport.sh /path/to/config.xml"
@@ -21,7 +22,6 @@ download(){
   local local_file="$M2_REPO/$group_id/$2/$3/$2-$3.jar"
   bootpath+=":"$local_file
 
-  echo $local_file
   if [ ! -f $local_file ]; then
     if wget --spider $URL 2>/dev/null; then
       echo "fetching $URL"
@@ -42,14 +42,14 @@ download(){
 }
 
 export scala_ver=2.13.10
-export scala3_ver=3.2.1
-export beangle_commons_ver=5.4.2
-export beangle_template_ver=0.1.2
-export slf4j_ver=2.0.3
-export logback_ver=1.4.4
-export commons_compress_ver=1.21
-export boot_ver=0.1.2
-export beangle_db_ver=0.0.17
+export scala3_ver=3.2.2
+export beangle_commons_ver=5.5.0
+export beangle_template_ver=0.1.4
+export slf4j_ver=2.0.6
+export logback_ver=1.4.5
+export commons_compress_ver=1.22
+export boot_ver=0.1.3
+export beangle_db_ver=0.0.18
 
 download org.scala-lang scala-library $scala_ver
 download org.scala-lang scala-reflect $scala_ver
